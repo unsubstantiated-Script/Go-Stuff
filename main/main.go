9b4dbedbb09d84cd7ddc5d82ff41ec7336489f17
unsubstantiated-Script <jusfulton@gmail.com>
2022-07-04 10:21:29 -0700

    some files updated

---
+++ main/main.go
@@ -0,0 +1,59 @@
+package main
+
+//func main() {
+//	var myString string
+//	myString = "Green"
+//
+//	log.Println("myString is set to", myString)
+//	changeUsingPointers(&myString)
+//	log.Println("after myString is set to", myString)
+//}
+//
+//func changeUsingPointers(s *string) {
+//	newValue := "Red"
+//	*s = newValue
+//}
+
+//type User struct {
+//	FirstName   string
+//	LastName    string
+//	PhoneNumber string
+//	Age         int
+//	BirthDate   time.Time
+//}
+//
+//func main() {
+//	user := User{
+//		FirstName:   "Bobo",
+//		LastName:    "Jenkins",
+//		PhoneNumber: "505-555-5555",
+//		Age:         55,
+//	}
+//
+//	log.Println(user.BirthDate)
+//}
+
+//type myStruct struct {
+//	FirstName string
+//}
+//
+//// Tying function to a struct
+//func (m *myStruct) printFirstName() string {
+//	return m.FirstName
+//}
+//
+//func main() {
+//	var myVar myStruct
+//	myVar.FirstName = "John"
+//
+//	myVar2 := myStruct{
+//		FirstName: "Mary",
+//	}
+//
+//	log.Println(myVar2.printFirstName())
+//	log.Println(myVar.printFirstName())
+//}
+
+func main() {
+
+}

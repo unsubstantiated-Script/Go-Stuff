1455fd0310e012b1dadd2f96716981dd2dd4d0b3
unsubstantiated-Script <jusfulton@gmail.com>
2022-07-04 15:20:24 -0700

    updates

--- main/main.go
+++ main/main.go
@@ -54,6 +54,74 @@ package main
 //	log.Println(myVar.printFirstName())
 //}
 
-func main() {
+//func main() {
+//	// Dunno what you want? Use interface as a placeholder
+//	myMap := make(map[string]string)
+//
+//	myMap["dog"] = "Skippy"
+//
+//	log.Println(myMap["dog"])
+//}
 
-}
+//func main() {
+//	//animals := []string{"dog", "fish", "horse", "cow", "cat"}
+//
+//	cakes := map[string]string{"drows": "chocolate", "binky": "carrot", "gangk": "strawberry"}
+//
+//	//Looping over a slice requires Pythonesque dictionary loops, but if you don't want to deal with the iterator, use an underscore
+//	for person, cake := range cakes {
+//		log.Println(person, cake)
+//	}
+//}
+//
+//type Animal interface {
+//	Says() string
+//	NumberOfLegs() int
+//}
+//
+//type Dog struct {
+//	Name  string
+//	Breed string
+//}
+//
+//type Gorilla struct {
+//	Name          string
+//	Color         string
+//	NumberOfTeeth int
+//}
+//
+//func main() {
+//	dog := Dog{
+//		Name:  "Sweetums",
+//		Breed: "German Shepherd",
+//	}
+//	PrintInfo(&dog)
+//
+//	gorilla := Gorilla{
+//		Name:          "Jock",
+//		Color:         "grey",
+//		NumberOfTeeth: 38,
+//	}
+//
+//	PrintInfo(&gorilla)
+//}
+//
+//func PrintInfo(a Animal) {
+//	fmt.Println("This animal says", a.Says(), "and has", a.NumberOfLegs(), "legs")
+//}
+//
+//func (d *Dog) Says() string {
+//	return "Woof"
+//}
+//
+//func (d *Dog) NumberOfLegs() int {
+//	return 4
+//}
+//
+//func (d *Gorilla) Says() string {
+//	return "Grunt"
+//}
+//
+//func (d *Gorilla) NumberOfLegs() int {
+//	return 2
+//}
